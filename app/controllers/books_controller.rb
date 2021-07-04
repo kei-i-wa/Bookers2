f382f68e9e4f7de493b12be793a666858782609e
kei-i-wa <kei.kumarg@gmail.com>
2021-07-04 06:16:41 +0000

    Update コメントの非同期通信

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,7 +26,7 @@ class BooksController < ApplicationController
   def show
     @books=Book.find(params[:id])
     @book=Book.new
-    @book_comment = BookComment
+    @book_comment = BookComment.new
     @book_comments = @book.book_comments.order(created_at: :desc)
 
   end
